549722bf754b048b50355310990f29ed8fe46065
carbbykid <carbbykid@gmail.com>
2022-08-27 22:53:37 +0700

    feat: init project-next

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>ChiPoPo</title>
+        <meta name="description" content="ChiPoPo" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>ChiPoPo</div>
+    </div>
+  );
+};
+
+export default Home;
