94d17ce7bdc576c7cc06cf66f8f2d7282333cc3f
chipopo <carbbykid@gmail.com>
2022-11-18 20:25:28 +0700

    fix: remove mongodb ver 2

--- src/pages/api/newsletter.ts
+++ src/pages/api/newsletter.ts
@@ -1,19 +1,19 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { connectToDatabase } from "util/mongodb";
+// import { connectToDatabase } from "util/mongodb";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
   const { method, body } = req;
-  const { db } = await connectToDatabase();
+  // const { db } = await connectToDatabase();
 
   if (method === "POST") {
     try {
-      const email = await db
-        .collection("emails")
-        .insertOne({ ...body, timestamp: Date.now() });
-      res.status(201).json(email);
+      // const email = await db
+      //   .collection("emails")
+      //   .insertOne({ ...body, timestamp: Date.now() });
+      // res.status(201).json(email);
     } catch (error) {
       res.status(500).json(error);
     }
